b36617058aef4979ad0923ef876a89240869fc4b
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-05-09 16:46:44 -0700

    feat(linked-lists): add flatten doubly linked list exercise

---
+++ data-structures-and-algorithms/algorithms/linked-lists/exercises/03.flattenMultiLevelDoublyLinkedList.js
@@ -0,0 +1,35 @@
+/**
+ * Given a doubly linked list, list nodes also have 
+ * a child property that can point to a separate doubly 
+ * linked list in addition to their next and previous properties.
+ * These child lists can also have doubly linked lists of their own, and so on.
+ * 
+ * Return the list as a single flattened doubly linked list. You are 
+ * given the head of the first level of the list.
+ * 
+ * https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+ */
+
+// Key points
+
+// 1. Verify constraints
+
+// 2. Test cases
+
+// 3. Brute force approach
+
+// 4. Code the brute fore
+
+// 5. Check for erros
+
+// 6. Test using test cases
+
+// 7. Space and time
+
+// 8. Optimize, get an optimal solution
+
+// 9. Code the optimal solution
+
+// 10. Test, new space and time 
+
+// 11. Validate this is okay with interviewer
